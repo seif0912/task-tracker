86e616eb04967d040dcb9ef943db52e15d40e98e
seif0912 <baddredineseif@gmail.com>
2022-05-13 23:34:12 +0100

    Updates

---
+++ precache-manifest.c1ecd4750bbec8578a95f48ba35ebff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cd74fa32b011ffbdc6b",
+    "url": "/seif0912/task-tracker.git/static/css/main.02390559.chunk.css"
+  },
+  {
+    "revision": "9cd74fa32b011ffbdc6b",
+    "url": "/seif0912/task-tracker.git/static/js/main.9cd74fa3.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/seif0912/task-tracker.git/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "82bbc439f268400c727c",
+    "url": "/seif0912/task-tracker.git/static/js/2.82bbc439.chunk.js"
+  },
+  {
+    "revision": "5b814fc80bd64d1392d4",
+    "url": "/seif0912/task-tracker.git/static/js/runtime~main.5b814fc8.js"
+  },
+  {
+    "revision": "042d459c196381e2ab7c70eaf26583a9",
+    "url": "/seif0912/task-tracker.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/seif0912/task-tracker.git/precache-manifest.c1ecd4750bbec8578a95f48ba35ebff0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/seif0912/task-tracker.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
